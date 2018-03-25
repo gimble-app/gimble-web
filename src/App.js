723847f774c32f5a6e071a47451a3bd8f37aa390
Dan Mutton <daniel.mutton@gmail.com>
2018-03-25 19:16:28 +0200

    Remove unused element

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import AppBar from 'material-ui/AppBar';
 import Toolbar from 'material-ui/Toolbar';
 import Typography from 'material-ui/Typography';
