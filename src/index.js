e898639e6a19ba2ab09cafdec8df5feb6b2b8ef2
Dan Mutton <daniel.mutton@gmail.com>
2018-04-08 15:13:04 +0200

    Throttle any save state calls

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import React from 'react';
 import { render } from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
+import throttle from 'lodash/throttle';
 import { loadState, saveState } from './storage';
 import rootReducer from './reducers';
 import App from './App';
@@ -17,9 +18,11 @@ const store = createStore(
   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 )
 
-store.subscribe(() => {
+const TIME_BETWEEN_UPDATES = 1000;
+
+store.subscribe(throttle(() => {
   saveState(store.getState())
-})
+}), TIME_BETWEEN_UPDATES);
 
 render(
   <Provider store={store}>
