91d4f1bb3455ddf28a1b7aeb31603cd7cac09105
Dan Mutton <daniel.mutton@gmail.com>
2018-04-13 08:52:12 +0200

    Fix naming of save button and accept an on click prop

--- src/event/SaveButton.js
+++ src/event/SaveButton.js
@@ -1,8 +1,12 @@
 import React from 'react';
 import Button from 'material-ui/Button';
+import { Link } from "react-router-dom";
 
-const AddButton = () => <Button color="inherit" aria-label="save" variant="flat">
-  Save
-</Button>
+const SaveButton = ({ onClick }) =>
+  <Link to="/">
+    <Button onClick={onClick} color="inherit" aria-label="save" variant="flat">
+      Save
+    </Button>
+  </Link>
 
-export default AddButton;
+export default SaveButton;
