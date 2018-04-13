0cb1ea487451c879b9ca5eb9624a1ada68444e6b
Dan Mutton <daniel.mutton@gmail.com>
2018-04-13 11:22:16 +0200

    Remove unrequired import

--- src/auth/LogoutMenuOption.test.js
+++ src/auth/LogoutMenuOption.test.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { shallow } from 'enzyme';
-import Button from 'material-ui/Button';
 import { LogoutMenuOption } from './LogoutMenuOption';
 import { MenuItem } from 'material-ui/Menu';
 
