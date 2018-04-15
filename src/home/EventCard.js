f0eb321cb465dd3e613bc996a4273bc9d004931e
Dan Mutton <daniel.mutton@gmail.com>
2018-04-15 21:00:20 +0200

    Remove details action as we do not need it yet

--- src/home/EventCard.js
+++ src/home/EventCard.js
@@ -1,6 +1,5 @@
 import React from 'react';
-import { CardActions, CardContent } from 'material-ui/Card';
-import Button from 'material-ui/Button';
+import { CardContent } from 'material-ui/Card';
 import { InternalLink } from '../common/InternalLinks';
 import { Card } from '../common/Cards';
 
@@ -8,9 +7,6 @@ const EventCard = ({ id, title }) => (
   <Card>
     <InternalLink to={`/event/${id}`}>
       <CardContent>{title}</CardContent>
-      <CardActions>
-        <Button size="small">Details</Button>
-      </CardActions>
     </InternalLink>
   </Card>
 );
