97b50a6b9bceb14c7dc8210fb32c55a739dcc792
Luděk Krůla <drcek.drtikol@gmail.com>
2018-04-15 17:08:23 +0200

    Changed links on event card

--- src/home/EventCard.js
+++ src/home/EventCard.js
@@ -1,17 +1,17 @@
 import React from 'react';
 import { CardActions, CardContent } from 'material-ui/Card';
 import Button from 'material-ui/Button';
-import { Link } from "react-router-dom";
+import { InternalLink } from '../common/InternalLinks';
 import { Card } from '../common/Cards';
 
 const EventCard = ({ id, title }) => (
   <Card>
-    <Link to={`/event/${id}`}>
+    <InternalLink to={`/event/${id}`}>
       <CardContent>{title}</CardContent>
       <CardActions>
         <Button size="small">Details</Button>
       </CardActions>
-    </Link>
+    </InternalLink>
   </Card>
 );
 
