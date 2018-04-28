8015b0d215af2640c4de34cae2afe077456d5539
Dan Mutton <YearOfTheDan@users.noreply.github.com>
2018-04-28 18:37:28 +0200

    Use jss provider to force styled-components classes to take precendence over material-ui ones

--- src/index.js
+++ src/index.js
@@ -5,19 +5,29 @@ import { render } from 'react-dom';
 import { Provider } from 'react-redux';
 import registerServiceWorker from './registerServiceWorker';
 import { MuiThemeProvider } from 'material-ui/styles';
+import JssProvider from 'react-jss/lib/JssProvider';
+import { create } from 'jss';
+import { createGenerateClassName, jssPreset } from 'material-ui/styles';
 import initStore from './store';
 import App from './App';
 import theme from './theme';
 
+const generateClassName = createGenerateClassName();
+const jss = create(jssPreset());
+jss.options.insertionPoint = document.getElementById('jss-insertion-point');
 const { store, history } = initStore();
 
+export default App;
+
 render(
-  <Provider store={store}>
-    <MuiThemeProvider theme={theme}>
-      <App history={history}/>
-    </MuiThemeProvider>
-  </Provider>,
-  document.getElementById('root')
+  <JssProvider jss={jss} generateClassName={generateClassName}>
+    <Provider store={store}>
+      <MuiThemeProvider theme={theme}>
+        <App history={history}/>
+      </MuiThemeProvider>
+    </Provider>
+  </JssProvider>,
+document.getElementById('root')
 );
 
 registerServiceWorker();
