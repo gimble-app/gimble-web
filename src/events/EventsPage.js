487df3f3b584821d9c082fa2762423ee3d6a08a7
Dan Mutton <YearOfTheDan@users.noreply.github.com>
2018-05-25 22:13:09 +0200

    Add a loading indicator to events

--- src/events/EventsPage.js
+++ src/events/EventsPage.js
@@ -1,19 +1,25 @@
 import React from 'react';
 import {compose} from "redux";
 import {connect} from "react-redux";
+import { LinearProgress } from 'material-ui/Progress';
+import {firebaseConnect, firestoreConnect, isLoaded, isEmpty} from "react-redux-firebase";
 import {selectEventsList} from "./selectors";
-import {firebaseConnect, firestoreConnect} from "react-redux-firebase";
 import {eventsForUserQuery} from "./firestoreQueries";
 import BackgroundMessage from './BackgroundMessage';
 import AddButton from "../common/AddButton";
 import Page from '../common/Page';
 import EventsOverview from './EventsOverview';
 
+
 export const EventsPage = ({ events }) => (
   <Page>
-    {events && events.length > 0
-      ? <EventsOverview events={events}/>
-      : <BackgroundMessage/>}
+    {
+      !isLoaded(events)
+        ? <LinearProgress />
+        : isEmpty(events)
+          ? <BackgroundMessage/>
+          : <EventsOverview events={events}/>
+    }
     <AddButton type="event"/>
   </Page>
 );
