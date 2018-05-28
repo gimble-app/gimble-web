06a4c4463b9bb9deeb328b704db642584c61e7aa
Dan Mutton <YearOfTheDan@users.noreply.github.com>
2018-05-28 21:41:09 +0200

    Make the InternalLink flex so that its content is centred

--- src/common/InternalLinks.js
+++ src/common/InternalLinks.js
@@ -2,6 +2,8 @@ import styled from 'styled-components';
 import { Link } from 'react-router-dom';
 
 export const InternalLink = styled(Link)`
+  display: inline-flex;
+  justify-content: center;
   color: inherit;
   text-decoration: none;
 `;
