722df46dd4d214e27996bc4ac472f14fe6c895b7
Dan Mutton <YearOfTheDan@users.noreply.github.com>
2018-06-01 22:32:17 +0200

    Add a border to the avatar

--- src/events/eventView/AvatarOrPlaceholder.js
+++ src/events/eventView/AvatarOrPlaceholder.js
@@ -1,10 +1,18 @@
 import React from 'react';
 import Avatar from "@material-ui/core/Avatar";
 import FaceIcon from '@material-ui/icons/Face';
+import {withTheme} from "@material-ui/core/styles";
+import styled from "styled-components/";
+import {fromPalette} from "../../theme";
+
+const BorderedAvatar = withTheme()(styled(Avatar)`
+  border: 1px solid ${({theme}) => fromPalette(theme, 'secondaryContrast')};
+`);
+
 
 const AvatarOrPlaceholder = ({ photoUrl }) => (
   photoUrl
-    ? <Avatar src={photoUrl} />
-    : <Avatar><FaceIcon /></Avatar>
+    ? <BorderedAvatar src={photoUrl} />
+    : <BorderedAvatar><FaceIcon /></BorderedAvatar>
 );
 export default AvatarOrPlaceholder;
