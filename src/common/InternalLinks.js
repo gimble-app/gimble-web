ee750a79f4d2f63a657f5555415ddbcae7b783d1
Dan Mutton <YearOfTheDan@users.noreply.github.com>
2018-06-02 15:30:58 +0200

    Make anchors inherit display to stop messing up the buttons

--- src/common/InternalLinks.js
+++ src/common/InternalLinks.js
@@ -2,7 +2,6 @@ import styled from 'styled-components';
 import { Link } from 'react-router-dom';
 
 export const InternalLink = styled(Link)`
-  display: inline-flex;
   justify-content: center;
   color: inherit;
   text-decoration: none;
