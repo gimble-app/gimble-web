c1c5ffd7d08945bdf1a2a88ae1e9ec992d734d09
Dan Mutton <YearOfTheDan@users.noreply.github.com>
2018-08-13 12:44:11 +0200

    Add DisplayText and replace in EventHeader

--- src/events/eventView/header/EventHeader.js
+++ src/events/eventView/header/EventHeader.js
@@ -2,12 +2,12 @@ import React from "react";
 import Header from "./Header";
 import HeaderContent from "./HeaderContent";
 import EventPageToolbar from "./EventPageToolbar";
-import HeaderTitleText from "../../../common/typography/HeaderText";
+import DisplayText from "../../../common/typography/DisplayText";
 
 const EventHeader = ({ event }) =>
   <Header square image={ event.image && event.image.src }>
     <EventPageToolbar event={ event }/>
-    <HeaderContent><HeaderTitleText>{ event.title }</HeaderTitleText></HeaderContent>
+    <HeaderContent><DisplayText>{ event.title }</DisplayText></HeaderContent>
   </Header>;
 
 export default EventHeader;
--- stories/Typography.js
+++ stories/Typography.js
@@ -5,12 +5,14 @@ import BodyText from "../src/common/typography/BodyText";
 import SubheadingText from "../src/common/typography/SubheadingText";
 import theme from "../src/theme";
 import TitleText from "../src/common/typography/TitleText";
+import DisplayText from "../src/common/typography/DisplayText";
 
 storiesOf('Typography', module)
 .addDecorator(
   muiTheme([theme]),
 )
 .add('body text', () => [
+  <DisplayText colour={"primary"}>This is DisplayText, used for page headers</DisplayText>,
   <TitleText colour={"primary"}>This is TitleText, used to title event list entries</TitleText>,
   <BodyText>This is BodyText, used for inputs, background messages, etc</BodyText>,
   <SubheadingText>This is SubheadingText, used as field headings</SubheadingText>
